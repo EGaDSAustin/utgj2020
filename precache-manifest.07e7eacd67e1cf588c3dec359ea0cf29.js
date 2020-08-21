ec4f6917f453f7498eca333116d5b488762d8e26
Colette Montminy <cmontminy@utexas.edu>
2020-08-20 19:14:37 -0500

    Updates

---
+++ precache-manifest.07e7eacd67e1cf588c3dec359ea0cf29.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4762f021c4bbb10d0705c942feb85f21",
+    "url": "/utgj2020/index.html"
+  },
+  {
+    "revision": "a1d693d1f099c82f0333",
+    "url": "/utgj2020/static/css/main.05b062f0.chunk.css"
+  },
+  {
+    "revision": "cfca31fd1be67b8d3000",
+    "url": "/utgj2020/static/js/2.3a92a5eb.chunk.js"
+  },
+  {
+    "revision": "18514a304b071425341cc13af3156b9f",
+    "url": "/utgj2020/static/js/2.3a92a5eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d693d1f099c82f0333",
+    "url": "/utgj2020/static/js/main.77d2538a.chunk.js"
+  },
+  {
+    "revision": "64ab67a046bfc83dff36",
+    "url": "/utgj2020/static/js/runtime-main.7418f90e.js"
+  },
+  {
+    "revision": "5e2b696cfe9f78a2701d8762b15686f6",
+    "url": "/utgj2020/static/media/Discord-Logo-Pure-White.5e2b696c.svg"
+  },
+  {
+    "revision": "68d3873ffdb48f2a442f12c5cddb6d11",
+    "url": "/utgj2020/static/media/apricot.68d3873f.png"
+  },
+  {
+    "revision": "2b3002d763608a2721c0543f7e1d052b",
+    "url": "/utgj2020/static/media/ascent.2b3002d7.png"
+  },
+  {
+    "revision": "dd9261c9075e0337e0d264b6eddd4b21",
+    "url": "/utgj2020/static/media/b-lock.dd9261c9.png"
+  },
+  {
+    "revision": "feb60a8549f0a9d543ac17bf8a7d83ed",
+    "url": "/utgj2020/static/media/blockadventures.feb60a85.png"
+  },
+  {
+    "revision": "8aa0beb2a45d018a20387bd9152da0ea",
+    "url": "/utgj2020/static/media/blockstar.8aa0beb2.gif"
+  },
+  {
+    "revision": "831b0fa57738e8e517d37b4787d85722",
+    "url": "/utgj2020/static/media/cozycatcafe.831b0fa5.png"
+  },
+  {
+    "revision": "5b469b17e4c070e2ee0e5a6eb79e10fe",
+    "url": "/utgj2020/static/media/fruitbanner.5b469b17.png"
+  },
+  {
+    "revision": "353ad61f3d25c2680f18ddde9f18da0d",
+    "url": "/utgj2020/static/media/garbagegladiators.353ad61f.png"
+  },
+  {
+    "revision": "83003c7c3ca63409664ec35bb91b0369",
+    "url": "/utgj2020/static/media/goodboye.83003c7c.png"
+  },
+  {
+    "revision": "0de004d1b41a8f0cddc8b09d303e1324",
+    "url": "/utgj2020/static/media/grape.0de004d1.png"
+  },
+  {
+    "revision": "6093a67b8bc344d767bbd1d1806e6d0b",
+    "url": "/utgj2020/static/media/ignite.6093a67b.png"
+  },
+  {
+    "revision": "304f53338ffdb51974188dcb1d1f5e7e",
+    "url": "/utgj2020/static/media/illegalaliens.304f5333.png"
+  },
+  {
+    "revision": "962b89e33db8f7e1c98499614c8d9c45",
+    "url": "/utgj2020/static/media/jamlogo.962b89e3.png"
+  },
+  {
+    "revision": "eef99161e44ced00f5c4dad05a988e7b",
+    "url": "/utgj2020/static/media/jellylong4.eef99161.png"
+  },
+  {
+    "revision": "cb2c85cc2add773e6b4931750fe8942a",
+    "url": "/utgj2020/static/media/junkyardbrawl.cb2c85cc.png"
+  },
+  {
+    "revision": "27045463110a842434e8b52feffe85ea",
+    "url": "/utgj2020/static/media/landfill.27045463.png"
+  },
+  {
+    "revision": "38978bc132719f5453a3b74ec4441f6f",
+    "url": "/utgj2020/static/media/littleblockers.38978bc1.png"
+  },
+  {
+    "revision": "e9df3d0e790abdfe972bd31babf8ac37",
+    "url": "/utgj2020/static/media/missionimpawsible.e9df3d0e.png"
+  },
+  {
+    "revision": "c7045472e7e7681a8c0e27d36e821e9b",
+    "url": "/utgj2020/static/media/orange.c7045472.png"
+  },
+  {
+    "revision": "b6b6616f8a666e94c4d34593e868b14e",
+    "url": "/utgj2020/static/media/pear.b6b6616f.png"
+  },
+  {
+    "revision": "defca96bd7eaf2276d72d13d2de689c1",
+    "url": "/utgj2020/static/media/pictureperfect.defca96b.png"
+  },
+  {
+    "revision": "827c2088e1021a1d6fddf5102de9f3b1",
+    "url": "/utgj2020/static/media/sponsorcollage.827c2088.png"
+  },
+  {
+    "revision": "9df94dd1d31b5998f9e85db1825a54ea",
+    "url": "/utgj2020/static/media/strawberry.9df94dd1.png"
+  },
+  {
+    "revision": "1a2f49a626afe62965d6961acce92e67",
+    "url": "/utgj2020/static/media/swiffersadventure.1a2f49a6.png"
+  },
+  {
+    "revision": "5816b6e948149ea997d2417b3fd02da4",
+    "url": "/utgj2020/static/media/tetrisblocker.5816b6e9.png"
+  },
+  {
+    "revision": "d2b85edea3488775d3714f6e6b907a51",
+    "url": "/utgj2020/static/media/trashdash.d2b85ede.png"
+  },
+  {
+    "revision": "ed818a54a2ea0af88298eb615323c149",
+    "url": "/utgj2020/static/media/trashketbrawl.ed818a54.png"
+  },
+  {
+    "revision": "1379fed2a85d4bd77e9cdc3c3d1527dd",
+    "url": "/utgj2020/static/media/trashteroids.1379fed2.gif"
+  },
+  {
+    "revision": "55e401f3ced28f2460aa8a0c38ff1923",
+    "url": "/utgj2020/static/media/zynga.55e401f3.png"
+  }
+]);
